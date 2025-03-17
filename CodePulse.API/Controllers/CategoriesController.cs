7177d1dfd4b2a6acef0859a0da775e8204f642b0
NeoCoderPro <adamrockey57@gmail.com>
2025-03-17 18:00:14 -0300

    feat: Add Update category action method HTTPPUT

--- CodePulse.API/Controllers/CategoriesController.cs
+++ CodePulse.API/Controllers/CategoriesController.cs
@@ -90,5 +90,36 @@ namespace CodePulse.API.Controllers
 
             return Ok(response);
         }
+
+        // PUT: https://localhost:7094/api/categories/{id}
+        [HttpPut]
+        [Route("{id:Guid}")]
+        public async Task<IActionResult> EditCategory([FromRoute] Guid id, UpdateCategoryRequestDto request)
+        {
+            // Convert DTO to Domain Model
+            var category = new Category
+            {
+                Id = id,
+                Name = request.Name,
+                UrlHandle = request.UrlHandle
+            };
+
+            category = await categoryRepository.UpdateAsync(category);
+
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            // Convert Domain Model to DTO
+            var response = new CategoryDto
+            {
+                Id = category.Id,
+                Name = category.Name,
+                UrlHandle = category.UrlHandle,
+            };
+
+            return Ok(response);
+        }
     }
 }
